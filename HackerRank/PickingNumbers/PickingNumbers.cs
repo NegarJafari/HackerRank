5259cb4323dc6d19f3f058e272315e269bf3fe1a
Negar <s.negar.jafari@gmail.com>
2023-12-13 01:55:51 +0330

    PickingNumbers

---
+++ HackerRank/PickingNumbers/PickingNumbers.cs
@@ -0,0 +1,29 @@
+﻿namespace HackerRank
+{
+    internal class PickingNumbers
+    {
+        public static int pickingNumbers(List<int> a)
+        {
+            var sortedList = a.Order().ToList();
+            var c = a.OrderBy(x => x).ToList();
+            var maxLenght = 0;
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                var count = 1;
+                var temp = sortedList[i];
+                for (int j = i + 1; j < sortedList.Count; j++)
+                {
+                    if (Math.Abs(temp - sortedList[j]) > 1)
+                    {
+                        break;
+                    }
+                    count++;
+                }
+                if (maxLenght < count)
+                    maxLenght = count;
+
+            }
+            return maxLenght;
+        }
+    }
+}
---
+++ HackerRank/PickingNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿using HackerRank;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var result = PickingNumbers.pickingNumbers(new List<int> { 4, 6, 5, 3, 3, 1 });
+        Console.WriteLine(result);
+    }
+}
