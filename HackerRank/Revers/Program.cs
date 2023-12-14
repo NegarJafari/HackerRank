8aafbf21cafeb547742b40f042f4e1c7c54c24c5
Negar <s.negar.jafari@gmail.com>
2023-12-15 01:05:39 +0330

    Revers

---
+++ HackerRank/Revers/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var result = Revers(100250);
+        Console.WriteLine(result);
+    }
+
+    private static int Revers(int n)
+    {
+        var reverse = 0;
+        var temp = 0;
+        while (n != 0)
+        {
+            temp = n % 10;
+            reverse = reverse * 10 + temp;
+            n /= 10;
+        }
+        return reverse;
+    }
+}
