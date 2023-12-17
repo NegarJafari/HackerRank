c29a20f4901acb1ff33e0af41c549aa3a1b26117
Negar <s.negar.jafari@gmail.com>
2023-12-18 02:13:13 +0330

    jumpingOnClouds

---
+++ HackerRank/JumpingOnTheClouds/Jump.cs
@@ -0,0 +1,48 @@
+﻿namespace JumpingOnTheClouds
+{
+    public class Jump
+    {
+        public int jumpingOnClouds(List<int> clouds)
+        {
+            var result = 0;
+            var lastIndex = clouds.Count - 1;
+
+            var counter = 0;
+            while (lastIndex > counter)
+            {
+                if (clouds[counter] == 0)
+                {
+                    if (counter + 2 <= lastIndex)
+                    {
+                        if (clouds[counter + 2] == 0)
+                        {
+                            result++;
+                            counter = counter + 2;
+                        }
+                        else if (clouds[counter + 1] == 0)
+                        {
+                            result++;
+                            counter = counter + 1;
+
+                        }
+                    }
+                    else if (counter + 1 <= lastIndex)
+                    {
+                        if (clouds[counter + 1] == 0)
+                        {
+                            result++;
+                            counter = counter + 1;
+
+                        }
+                    }
+                }
+                else
+                {
+                    counter++;
+                }
+
+            }
+            return result;
+        }
+    }
+}
---
+++ HackerRank/JumpingOnTheClouds/Program.cs
@@ -0,0 +1,13 @@
+﻿using JumpingOnTheClouds;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var jump = new Jump();
+        var input = new List<int>() { 0,0,1,0,0,0,1,0};
+        var result = jump.jumpingOnClouds(input);
+        Console.WriteLine(result);
+        //retrun 19:05:45
+    }
+}
